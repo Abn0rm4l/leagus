644691c7241aec8d23981244c811ad1442886065
Lionel Guimaraes <inv3nt@gmail.com>
2024-02-25 22:11:35 +0200

    Add the leagus-web-app project based on the SvelteKit starter
    I have added two pages and a few simple components using Flowbite-Sevelte.

---
+++ leagus-web-app/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.pcss';
+	import { page } from '$app/stores';
+	import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Leagus Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Leagus</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl {activeUrl}>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/leagues">Leagues</NavLi>
+		<NavLi href="/settings">Settings</NavLi>
+	</NavUl>
+</Navbar>
+
+<slot />
