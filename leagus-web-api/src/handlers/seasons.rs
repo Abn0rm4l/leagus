6918199b8cec9ac59d16cb90a2234df685fc2e43
Lionel Guimaraes <inv3nt@gmail.com>
2024-03-16 13:57:53 +0200

    Add the first working version of the create season dialog
    Still work to do but at least it's functional.

--- leagus-web-api/src/handlers/seasons.rs
+++ leagus-web-api/src/handlers/seasons.rs
@@ -1,13 +1,22 @@
+use std::str::FromStr;
+
 use askama::Template;
-use axum::{response::Html, routing::get, Json, Router};
-use leagus::persistence::{mongo_store::MongoStore, WriteableStore};
+use axum::{extract::Path, response::Html, routing::get, Form, Json, Router};
+use bson::Uuid;
+use chrono::{DateTime, DurationRound, NaiveDate, NaiveDateTime, TimeDelta, Utc};
+use leagus::{
+    models::{League, Season},
+    persistence::{mongo_store::MongoStore, WriteableStore},
+};
+use serde::Deserialize;
 use serde_json::Value;
 
 /// Routes available for '/seasons' path.
 pub fn routes() -> Router {
-    Router::new()
-        .route("/", get(list))
-        .route("/create", get(create_season_modal))
+    Router::new().route("/", get(list)).route(
+        "/create/:league_id",
+        get(get_create_season).post(post_create_season),
+    )
 }
 
 /// List all seasons
@@ -21,12 +30,67 @@ pub async fn list() -> Json<Value> {
     Json(serde_json::to_value(seasons).unwrap())
 }
 
-pub async fn create_season_modal() -> Html<String> {
-    Html(SeasonCreateModalTemplate { title: "None" }.to_string())
+pub async fn get_create_season(Path(league_id): Path<Uuid>) -> Html<String> {
+    let store = MongoStore::new().await.unwrap();
+    // TODO: handle when no league is found
+    let league = store.get_league(&league_id).await.unwrap();
+
+    Html(SeasonCreateModalTemplate { league }.to_string())
+}
+
+pub async fn post_create_season(
+    Path(league_id): Path<Uuid>,
+    Form(create_season): Form<CreateSeasonInput>,
+) -> Html<String> {
+    // TODO: Maybe store these as NaiveDate, I don't think any value is gained from having it as
+    // DateTime?
+
+    // Try parse the Date, if that doesn't work use Utc::now.
+    let start = NaiveDate::parse_from_str(&create_season.start_date, "%Y-%m-%d");
+    let start = match start {
+        Ok(start) => NaiveDateTime::from(start).and_utc(),
+        Err(_) => Utc::now().duration_trunc(TimeDelta::days(1)).unwrap(),
+    };
+
+    // If no end date is included we set it to +30 days from start
+    // TODO: Make end date optional
+    let end = NaiveDate::parse_from_str(&create_season.end_date, "%Y-%m-%d");
+    let end = match end {
+        Ok(end) => NaiveDateTime::from(end).and_utc(),
+        Err(_) => (Utc::now() + TimeDelta::days(30))
+            .duration_trunc(TimeDelta::days(1))
+            .unwrap(),
+    };
+
+    let name = if create_season.season_name.is_empty() {
+        // Default name will be "Month - Year".
+        start.format("%B - %Y").to_string()
+    } else {
+        create_season.season_name
+    };
+
+    let mut store = MongoStore::new().await.unwrap();
+    let season = Season::new(&league_id, &start, &end, &name);
+    store.create_season(&season).await;
+
+    //TODO: return something more useful
+    Html(format!("{:?}", season))
 }
 
 #[derive(Template)]
 #[template(path = "partials/season_create_modal.html")]
-struct SeasonCreateModalTemplate<'a> {
-    title: &'a str,
+struct SeasonCreateModalTemplate {
+    league: League,
+}
+
+#[derive(Deserialize)]
+pub struct CreateSeasonInput {
+    #[serde(default)]
+    season_name: String,
+    #[serde(default)]
+    start_date: String,
+    #[serde(default)]
+    end_date: String,
+    // #[serde(default)]
+    // make_current: bool,
 }
